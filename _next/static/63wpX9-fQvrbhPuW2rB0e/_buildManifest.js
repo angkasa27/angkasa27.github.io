b1b2f665a50f30c8162a5b3010703068e95f780c
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2022-04-24 21:12:27 +0700

    Clean up gh-pages

--- _next/static/63wpX9-fQvrbhPuW2rB0e/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-c2ffd8d428a8879e.js","static/chunks/189-dd4c8645caedb0ef.js","static/chunks/pages/index-35bf154e84c6f6e5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
