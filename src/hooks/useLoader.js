60c410ea9a791a5f7886d25e9f7c68d09b4d6a8b
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2022-04-24 20:47:31 +0700

    Remove old prioject

--- src/hooks/useLoader.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export default function useLoader() {
-  const [isLoading, setIsLoading] = useState(0);
-
-  console.log(isLoading);
-  return [isLoading, setIsLoading];
-}
